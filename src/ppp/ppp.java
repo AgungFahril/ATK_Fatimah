d8f4b3435fcf8b42941d69bc9b812c044a7e6160
E31240699_Agung Fahril Gunawan_Gol B <agung.fahril.gunawan@gmail.com>
2025-05-19 20:59:13 +0700

    Project ATK_Fatimah

---
+++ src/ppp/ppp.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ppp;
+
+/**
+ *
+ * @author Agung Fahril Gunawan
+ */
+public class ppp {
+    
+}
